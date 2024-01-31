0122068ce1a17ef4640c1dc64321f5175b2b3ef2
Yuhei Yoshida <yyouhey617@gmail.com>
2024-01-31 17:44:17 +0900

    FirstCommit

---
+++ src/main/java/com/example/todoCalendar/TodoCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoCalendarApplication.class, args);
+	}
+
+}
